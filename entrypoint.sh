9d0dba625302482ec670d9bf838593a02c0e59a9
jslim01 <jslim@uniai.co.kr>
2025-03-17 16:46:43 +0900

    스트림 3개로 셋팅

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ffmpeg -re -stream_loop -1 -i /videos/video1.mp4 \
+ffmpeg -re -stream_loop -1 -i ${VIDEO_PATH} \
        -vf "scale=1280:720,format=yuv420p" \
        -c:v libx264 -preset veryfast -b:v 1500k -maxrate 2000k -bufsize 3000k \
        -g 30 -keyint_min 30 \
        -rtsp_transport tcp \
        -an \
-       -f rtsp rtsp://rtsp-server:8554/stream
+       -f rtsp rtsp://rtsp-server:8554${RTSP_PATH}
